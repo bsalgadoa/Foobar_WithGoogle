4ca6761913509b58c8f68e4c84b613f8350fdcac
bsalgadoa <b.salgado.a@gmail.com>
2022-07-06 18:38:45 +0100

    fix: solve Google Foobar d2p1 and pass all test cases

--- 002_gearing_up_for_destruction.py
+++ 002_gearing_up_for_destruction.py
@@ -46,72 +46,111 @@ Output:
     -1,-1
 '''
 
+## References:
+    # https://docs.python.org/3/library/fractions.html
+    # https://realpython.com/python-fractions/
+
+## Notes:
+    # r is always the radius of the first gear, r = r0
+    # Python 2.7.13 sandbox (!)
+
+## Line of tought:
+    # First I tried to solve this problem for 3 gears (p[0], p[1] and p[2])
+        # considering p[2]-p[0] = 3r2 + 2r1 and it didn't went as expected
+        # so instead of considering 3r2, I tried to solve it again replacing 3r2 for r + r2 (r = 2r2)
+        # total distance = p[2]-p[0] = 2r + 2r1 + r2
+        # from here I deducted that:
+            # d0 = p[1] - p[0] = r + r1
+            # d1 = p[2] - p[1] = r1 + r2
+            # dn = p[n+1] - p[n] = rn + rn+1
+            # and we know that r = 2r2 (for n=2), so r = 2rn (for n)
+                # this means that we can always have n equations to n gears and therefore solve it to r
+                # using 3 gears for demosntration:
+                    # p[1] - p[0] = r + r1      r1 = p[1] - p[0] - 2r2
+                    # p[2] - p[1] = r1 + r2 <=> r2 = p[2] - 2p1 + p[0] + 2r2
+                    # r = r2                    r = 2 * (-p[0] + 2p[1] - p[2]) <<<
+
+# Then I solved for 4 gears, 5 and 6, and noticed that
+# a patern emerges in r when the number of gears is odd or even:
+    # len == 3:
+    #     r = 2 * (-p[0] + 2p[1] - p[2])
+    # len == 4:
+    #     r = 2/3 * (-p[0] + 2p[1] - 2p[2] + p[3])
+    # len == 5:
+    #     r = 2 * (-p[0] + 2p[1] - 2p[2] + 2p[3] - p[4])
+    # len == 6:
+    #     r = 2/3 * (-p[0] + 2p[1] - 2p[2] + 2p[3] - 2p[4] + p[5])
+
+# Therefore we can simplify and state that if lenght is:
+    # even:
+        # r = 2/3 * (-p[0] + 2*(sum p1_to_pn_minus2) + p[-1])
+    # odd:
+        # r = 2 * (-p[0] + 2*(sum p1_to_pn_minus2) + p[-1])
+
+# Now that we've found a way to determine r, in order to determine if its possible and return the solution
+    # we must check first if:
+        # r >= 2, because the minimum radius is 1 and r = 2rn.
+        # and also that all the gears have radius >= 1.
+            # note: overlaping gears or gears that don't touch, means that there will be gears with radius < 1.
+
+    # if so, return the r in a fraction in its simple form.
+
+
 from fractions import Fraction
 
 def solution(pegs):
 
+    ## only two gears:
     if len(pegs) == 2:
-        # keeping in mind that we have "gears of any size, from a radius of 1 on up"
-        # and that r0 = 2rn, we know that r0 must be at least 2 and rn at least 1.
-        # therefore, in order to acomodate the minimum gears possible, the diference between p0 and p1 must be at least 3.
-            ## wich is the same of checking if r >= 2.
-        if (pegs[1] - pegs[0]) >= 3:
-            r = 2/3 * (pegs[1] - pegs[0])
-            r = Fraction(r).limit_denominator()
-            return [r.numerator, r.denominator]
-        # if it's not:
-        else:
+        # r == 2/3 and r1 == 1/3 of the distance between gears
+        r = 2/3.0 * (pegs[1] - pegs[0])
+
+        # and r must be > 2 ->> "(...)from a radius of 1 on up"
+        if r < 2:
             return [-1, -1]
 
 
-    else: #len list >=3
-        len_pegs = len(pegs)
-        s = 1
-        p = pegs
-        p1_to_pn_minus2 = 0
+    ## more than 2 gears:
+    else:
+        p = pegs # readability simplification.
+        p1_to_pn_minus2 = 0 # store the sum of p's, for every p between p[1] to p[n-2].
+        s = 1 # we'll use this to invert the signal while calculating p1_to_pn_minus2.
 
-        for i in range(1,len_pegs-1):
+        # determine p1_to_pn_minus2:
+        for i in range(1,len(pegs)-1):
             p1_to_pn_minus2 += s * p[i]
+            # p1_to_pn_minus2 sum inverts the signal (+ or -) for every p[n] between p[1] to p[n-2].
+            # we'll use -1s to do so.
             s *= -1
 
-        if len_pegs % 2: # Odd - IMPAR
+        # now we can determine r:
+        if len(pegs) % 2: # odd
             r = 2 * (-p[0] + 2*(p1_to_pn_minus2) - p[-1])
-            #r = Fraction(r).limit_denominator()
 
-        else: # Even - PAR
-            r = 2/3 * (-p[0] + 2*(p1_to_pn_minus2) + p[-1])
-            #r = Fraction(r).limit_denominator()
+        else: # even
+            r = 2/3.0 * (-p[0] + 2*(p1_to_pn_minus2) + p[-1])
 
-        # now that we now r we must check if it's bigger than 2 like we do when len == 2.
+        # again r must allways be > 2 ->> "(...)from a radius of 1 on up"
         if r < 2:
             return [-1,-1]
 
-        # now we only need to check if the gears touch eachother and don't overlap
-        else:
-            #print ("r :", r)
-            ri = r
-            for i in range(1, len_pegs):
-                #print ("ri : ", ri)
-                #print ("i :", i)
-                ri = p[i] - p[i-1] - ri
-                #print ("rii :", rii)
-###!!!!
-                if ri < 1 or "cross the next!":
-                    return [-1,-1]
+        # now we only need to check if all the other gears are >= 1
+        ri = r
+        for i in range(1, len(pegs) ):
+            ri = p[i] - p[i-1] - ri
+            if ri < 1:
+                return [-1,-1]
 
-        #print ("r : ", r)
-        #print ("ri : ", ri)
-        #if (r/2 != ri):
-        #    print('over')
-        #    return [-1,-1]
+    # if the configuration is possible
+    # we convert r to a fraction "in its simplest form" using limit_denominator:
+    r = Fraction(r).limit_denominator()
 
+    return [r.numerator, r.denominator]
 
-        r = Fraction(r).limit_denominator()
-        return [r.numerator, r.denominator]
 
 
 if __name__ == '__main__':
     #solution()
     #import timeit as t
-    print("solution:", solution([0, 40, 80, 90]))
+    print("solution:", solution([10,50,80,87.5]))
     #print(t.timeit(solution, number=100))
